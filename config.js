4bfcf8801190399aaa49873a120c976ab1e59530
sakagawa <stream.yasuaki@gmail.com>
2026-05-20 00:25:50 +0900

    config.jsを除外

--- config.js
+++
@@ -1,5 +0,0 @@
-// このファイルは .gitignore で除外されています。
-// Apps Script Webアプリの /exec URL を window.APPS_SCRIPT_URL に設定してください。
-// 空文字列のままだとローカルの sample-words.json をフォールバックとして使用します。
-
-window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyhL8QgWblBUxCFb5idoAkgQl3sUcHGBI4zOHtgk1FKDOrEByvSWvysJOtd6wMe9DbjYw/exec";
